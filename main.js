1d29d9c1949506c1080ba2b265321546159a0a92
ve-suzuki15 <145079885+ve-suzuki15@users.noreply.github.com>
2025-08-01 21:21:09 -0300

    ok

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-05T22:03:10");
+const tempoObjetivo2 = new Date("2026-12-05T06:07:08");
+const tempoObjetivo3 = new Date("2027-12-30T07:05:04");
+const tempoObjetivo4 = new Date("2026-12-01T00:08:05");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
